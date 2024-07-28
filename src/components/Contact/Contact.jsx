68ddefc6dcd5d3c195ed90c38a92d371c0d41c65
KoliaKosyanenko <kosianenkomykola@gmail.com>
2024-07-28 19:01:05 +0300

    added edit contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,40 +4,62 @@ import { FaUser, FaPhoneAlt } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { deleteContacts } from '../../redux/contacts/operations';
 import DeleteModal from '../DeleteModal/DeleteModal'
+import EditContactModal from '../EditContactModal/EditContactModal';
 
 export default function Contact({ data: { id, name, number } }) {
   const dispatch = useDispatch();
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [contactData, setContactData] = useState({ id, name, number });
 
-  const handleOpenModal = () => setIsModalOpen(true);
-  const handleCloseModal = () => setIsModalOpen(false);
+  const handleOpenDeleteModal = () => setIsDeleteModalOpen(true);
+  const handleCloseDeleteModal = () => setIsDeleteModalOpen(false);
+
+  const handleOpenEditModal = () => setIsEditModalOpen(true);
+  const handleCloseEditModal = () => setIsEditModalOpen(false);
+
+  const handleEditContact = (values) => {
+    setContactData({ ...contactData, name: values.name, number: values.phone });
+    handleCloseEditModal();
+  };
 
   const handleDeleteContact = () => {
     dispatch(deleteContacts(id));
-    handleCloseModal();
+    handleCloseDeleteModal();
   };
 
   return (
     <div className={css.contactContainer}>
-      <div className={css.title}>
-        <p className={css.userName}>
-          <FaUser className={css.icon} />
-          {name}
-        </p>
-        <div className={css.separator} />
-        <p>
-          <FaPhoneAlt className={css.icon} />
-          {number}
-        </p>
-      </div>
-      <button className={css.btn} onClick={handleOpenModal}>
+    <div className={css.title}>
+      <p className={css.userName}>
+        <FaUser className={css.icon} />
+        {contactData.name}
+      </p>
+      <div className={css.separator} />
+      <p>
+        <FaPhoneAlt className={css.icon} />
+        {contactData.number}
+      </p>
+    </div>
+    <div className={css.buttonContainer}>
+      <button className={css.editBtn} onClick={handleOpenEditModal}>
+        Edit
+      </button>
+      <button className={css.btn} onClick={handleOpenDeleteModal}>
         Delete
       </button>
-      <DeleteModal 
-        open={isModalOpen} 
-        handleClose={handleCloseModal} 
-        handleDelete={handleDeleteContact} 
-      />
     </div>
+    <DeleteModal 
+      open={isDeleteModalOpen} 
+      handleClose={handleCloseDeleteModal} 
+      handleDelete={handleDeleteContact} 
+    />
+    <EditContactModal 
+      open={isEditModalOpen} 
+      handleClose={handleCloseEditModal} 
+      handleSave={handleEditContact} 
+      initialValues={{ name: contactData.name, phone: contactData.number }} 
+    />
+  </div>
   );
 }
